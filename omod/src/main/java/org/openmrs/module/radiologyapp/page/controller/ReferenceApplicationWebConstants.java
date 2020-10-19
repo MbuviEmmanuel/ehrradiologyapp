c3a988834a9ed70e5f80998e2fc0666dde6f78a0
MbuviEmmanuel <emmambu6@gmail.com>
2020-10-19 10:24:30 +0300

    Initial setup for radiology to suit KenyaEMR platform

---
+++ omod/src/main/java/org/openmrs/module/radiologyapp/page/controller/ReferenceApplicationWebConstants.java
@@ -0,0 +1,10 @@
+package org.openmrs.module.radiologyapp.page.controller;
+
+public class ReferenceApplicationWebConstants {
+    public static final java.lang.String SESSION_ATTRIBUTE_INFO_MESSAGE = "_REFERENCE_APPLICATION_INFO_MESSAGE_";
+    public static final java.lang.String SESSION_ATTRIBUTE_ERROR_MESSAGE = "_REFERENCE_APPLICATION_ERROR_MESSAGE_";
+    public static final java.lang.String SESSION_ATTRIBUTE_REDIRECT_URL = "_REFERENCE_APPLICATION_REDIRECT_URL_";
+    public static final java.lang.String REQUEST_PARAMETER_NAME_REDIRECT_URL = "redirectUrl";
+    public static final java.lang.String COOKIE_NAME_LAST_SESSION_LOCATION = "referenceapplication.lastSessionLocation";
+
+}
